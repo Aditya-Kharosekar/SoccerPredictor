97d55f2d5fdff17a653b02515d23746268dcb163
Aditya Kharosekar <adityakharosekar@gmail.com>
2016-08-12 11:28:09 +0530

    Changed structure of printTeamOptions function

--- SoccerPredictor/SoccerPredictor.py
+++ SoccerPredictor/SoccerPredictor.py
@@ -2,21 +2,36 @@ from openpyxl import load_workbook
  
 teams = []
 
-#homeAway is a flag that tells the function if it is printing the home team options or the away team options. 1 if home team. 0 if away
-#This matters because while printing the away team options, I don't want to show the already selected home team as an option
-#Example: if team A is the home team, you should not be able to pick Team A again as the away team
-#This function will print the appropriate team choices, and return the index of the team chosen
-def printTeamOptions(homeAway):
+#This function will print the appropriate team choices, and return a list. The first element will be the index of the home team.
+#The second element in the list will be the index of the away team
+def printTeamOptions():
+    result = []     #This will be the list that the function returns
+
     wb = load_workbook("Soccer Points Database.xlsx", data_only=True)
     sheet = wb["Sheet1"]
-    for num in range(0, 20):
+
+    for num in range(0, 20):        #putting all 20 team names into the list teams[]
         cell_num = "A"+str((num+1))
         teams.append(sheet[cell_num].value)
 
+    for num in range (0, 20):       #printing the home team options
+        print(str(num+1) + ". " + teams[num])
+    print("Enter index: ")
+    home = int(input())
+    result.append(home)
+    
+    for num in range(0, 20):        #printing the away team options
+       if (num!=home):
+           print(str(num+1) + ". " + teams[num])
+    print("Enter index: ")
+    away = int(input())
+    result.append(away)  
+    return result                 
+
 def main():
-    print("Enter home team: ")
-    printTeamOptions(1) #So that it prints options for the home team
-    print(teams)
+    print("Enter index of home team: ")
+    teamChoices = printTeamOptions()
+    print(teamChoices)
 
 if __name__ == '__main__':
     main()
